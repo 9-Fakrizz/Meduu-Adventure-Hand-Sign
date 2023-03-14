a7ee028c61dd88d32e895fc2bf1e442431e9d160
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-03-14 11:36:48 +0700

    Using Face-detection

--- Camera.cpp
+++ Camera.cpp
@@ -1,23 +1,40 @@
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-///////////////// Webcam //////////////////////
 
-int main() {
+///////////////  Images  //////////////////////
 
-    VideoCapture cap(0);
-    Mat img;
+void main() {
 
-    while (true) {
+	VideoCapture video(0);
+	CascadeClassifier facedetect;
+	Mat img;
+	facedetect.load("haarcascade_frontalface_default.xml");
 
-        cap.read(img);
-        imshow("Image", img);
-        waitKey(1);
+	while (true) {
+		video.read(img);
 
-    }
-    return 0;
+		vector<Rect> faces;
+
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+
+		cout << faces.size() << endl;
+
+		for (int i = 0; i < faces.size(); i++) {
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+			rectangle(img, Point(0, 0), Point(250, 70), Scalar(50, 50, 255), FILLED);
+			putText(img, to_string(faces.size()) + " Face Found", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
+		}
+
+
+		imshow("Frame", img);
+		waitKey(1);
+
+	}
 }
